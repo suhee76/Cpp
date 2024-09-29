e3ea7b2fd0c97fc9c048730dd9710cb2d5399e6d
suhee76 <wkdtngml02@naver.com>
2024-09-29 17:15:18 +0900

    main.cpp

--- HW/hw#1/main.cpp
+++ HW/hw#1/main.cpp
@@ -9,5 +9,5 @@ int main() {
 	ram.write(101, 30);
 	char res = ram.read(100) + ram.read(101);
 	ram.write(102, res);
-	cout << "102 ¹øÁöÀÇ °ª = " << (int)ram.read(102) << endl;
+	cout << "102 ë²ˆì§€ì˜ ê°’ = " << (int)ram.read(102) << endl;
 }
